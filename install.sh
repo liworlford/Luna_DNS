81ede1f2b358e8b10b594cd41265075307e5a225
Wolford Li <wolfordli@outlook.com>
2024-10-02 09:34:21 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ wget -O nf https://github.com/sjlleo/netflix-verify/releases/download/v3.1.0/nf_
 curl -L -o check.sh https://raw.githubusercontent.com/liworlford/Luna_DNS/refs/heads/main/check.sh
 curl -L -o major.py https://raw.githubusercontent.com/liworlford/Luna_DNS/refs/heads/main/major.py
 curl -L -o d.sh https://raw.githubusercontent.com/liworlford/Luna_DNS/refs/heads/main/d.sh
+systemctl stop systemd-resolved && systemctl disable systemd-resolved && rm -rf /etc/resolv.conf && echo 'nameserver 8.8.8.8'>/etc/resolv.conf
 
 # 检查 dnsmasq 是否正在运行
 if pgrep -x "dnsmasq" > /dev/null
