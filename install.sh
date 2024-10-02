5d3fa96126993602b6874daf34f377b166709631
Wolford Li <wolfordli@outlook.com>
2024-10-02 09:41:22 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@ wget -O nf https://github.com/sjlleo/netflix-verify/releases/download/v3.1.0/nf_
 curl -L -o check.sh https://raw.githubusercontent.com/liworlford/Luna_DNS/refs/heads/main/check.sh
 curl -L -o major.py https://raw.githubusercontent.com/liworlford/Luna_DNS/refs/heads/main/major.py
 curl -L -o d.sh https://raw.githubusercontent.com/liworlford/Luna_DNS/refs/heads/main/d.sh
+curl -L -o d.py https://raw.githubusercontent.com/liworlford/Luna_DNS/refs/heads/main/d.py
 systemctl stop systemd-resolved && systemctl disable systemd-resolved && rm -rf /etc/resolv.conf && echo 'nameserver 8.8.8.8'>/etc/resolv.conf
 bash d.sh
