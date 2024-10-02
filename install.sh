7a735e9c7f0325691607fa2b1c52cc9b95ae7050
Wolford Li <wolfordli@outlook.com>
2024-10-02 09:12:55 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,14 @@
 wget --no-check-certificate -O dnsmasq_sniproxy.sh https://raw.githubusercontent.com/myxuchangbin/dnsmasq_sniproxy_install/master/dnsmasq_sniproxy.sh && bash dnsmasq_sniproxy.sh -f
 wget -O nf https://github.com/sjlleo/netflix-verify/releases/download/v3.1.0/nf_linux_amd64 && chmod +x nf && ./nf
-curl -L -o check.sh https://raw.githubusercontent.com/WolfordLi/nfdns/main/check.sh
-curl -L -o main.py https://raw.githubusercontent.com/WolfordLi/nfdns/main/main.py
-curl -L -o pause.py https://raw.githubusercontent.com/WolfordLi/nfdns/main/pause.py
-curl -L -o continue.py https://raw.githubusercontent.com/WolfordLi/nfdns/main/continue.py
-python3 main.py
+curl -L -o check.sh https://raw.githubusercontent.com/liworlford/Luna_DNS/refs/heads/main/check.sh
+curl -L -o major.py https://raw.githubusercontent.com/liworlford/Luna_DNS/refs/heads/main/major.py
+curl -L -o d.sh https://raw.githubusercontent.com/liworlford/Luna_DNS/refs/heads/main/d.sh
+
+# 检查 dnsmasq 是否正在运行
+if pgrep -x "dnsmasq" > /dev/null
+then
+    echo "dnsmasq is running"
+else
+    echo "dnsmasq is not running"
+    bash d.sh
+fi
