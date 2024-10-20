e5145aa7d2388be929dbf11b2a74f4691b9d7b13
Wolford Li <wolfordli@outlook.com>
2024-10-20 16:13:01 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,9 +6,13 @@ curl -L -o d.sh https://raw.githubusercontent.com/liworlford/Luna_DNS/refs/heads
 curl -L -o d.py https://raw.githubusercontent.com/liworlford/Luna_DNS/refs/heads/main/d.py
 if systemctl start dnsmasq 2>/dev/null; then
     echo "dnsmasq started successfully."
+    echo "请注意 如果不是初次安装或更新 只是更改地区解锁 请运行python3 major.py节约时间"
     python3 major.py
 else
     echo "Failed to start dnsmasq. It may not be installed."
     python3 d.py
+    echo "dnsmasq started successfully."
+    echo "更新脚本请运行bash install.sh"
+    echo "更改地区请运行python3 major.py"
     python3 major.py
 fi
